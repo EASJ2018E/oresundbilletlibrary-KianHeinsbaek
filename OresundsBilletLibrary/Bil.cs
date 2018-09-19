5c1f9aaec6113425bcaac69ccaa423f0e73bcaca
Kian Heinsbæk <k.heinsbaek@gmail.com>
2018-09-19 10:27:21 +0200

    Opgave 10

---
+++ OresundsBilletLibrary/Bil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BilletLibrary;
+
+namespace OresundsBilletLibrary
+{
+    public class Bil : KøretøjSuperClass
+    {
+        public override double Pris()
+        {
+            return 410;
+        }
+
+        public override string Køretøj()
+        {
+            return "Øresund Bil";
+        }
+
+        public override double BroBizz()
+        {
+            return 161;
+        }
+    }
+}
---
+++ OresundsBilletLibrary/MC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BilletLibrary;
+
+namespace OresundsBilletLibrary
+{
+    class MC : KøretøjSuperClass
+    {
+        public override double Pris()
+        {
+            return 210;
+        }
+
+        public override string Køretøj()
+        {
+            return "Øresund MC";
+        }
+
+        public override double BroBizz()
+        {
+            return 73;
+        }
+    }
+}
